ae7d55a7cc1f25c2d05ce7fbc10b68723561f5d9
Your Name <yusuf@mesibo.com>
2021-12-18 21:34:35 +0530

    update

--- ios/Swift/FirstAppSwift/register.sh
+++
@@ -1,3 +0,0 @@
-# Authentication token is invalid or unverified. Either verify it with the email that was sent or register a new session.
-# reregister if above error is received. 
-pod trunk register contact@mesibo.com 'mesibo'
